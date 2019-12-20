d52880ea939cf92dbba26fd93780d8d96edf684e
jeeva <35072190+jeevananthamp@users.noreply.github.com>
2019-12-20 12:21:25 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ if(process.env.NODE_ENV === 'production')
 module.exports={
     mongoURI:'mongodb+srv://kitkat:thbs123@cluster0-din5z.mongodb.net/test?retryWrites=true&w=majority'
 }
+    console.log("Connected to databases");
 }
 else{
     module.exports={
